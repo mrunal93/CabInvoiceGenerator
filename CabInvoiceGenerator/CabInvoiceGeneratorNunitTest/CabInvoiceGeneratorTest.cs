ad5b87c16dc60b258725c3987a65333005505a4a
mrunal93 <mrn26893@gmail.com>
2020-08-11 20:12:24 +0530

    [Mrunal]Add: The welcome message and .gitignore file for cabinvoice GEnerator

---
+++ CabInvoiceGenerator/CabInvoiceGeneratorNunitTest/CabInvoiceGeneratorTest.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace CabInvoiceGeneratorNunitTest
+{
+    public class CabInvoiceGeneratorTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
